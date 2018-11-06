e91337f8d54c9cc1bcae24db667006ba6637c703
jens-daniel-mueller <jens.mueller@io-warnemuende.de>
2018-11-07 00:01:44 +0100

    Plot only zeroing data, without first 20 or so
    to be finished

--- Read_HydroC_Raw_data_files.R
+++ Read_HydroC_Raw_data_files.R
@@ -1,14 +1,18 @@
 library(tidyverse)
+library(lubridate)
 library(data.table)
-
+library(plotly)
 
 setwd("C:/Mueller_Jens_Data/180530_BloomSail/Data/Sensor/HydroC-pCO2")
 files <- list.files(pattern = "*.txt")
 
 df <- files %>%
-  map(read.delim, sep=";", skip=4, col.names = seq(1,23,1)) %>%
+  map(read.delim, sep=";", dec=",", skip=4, col.names = seq(1,23,1),
+      colClasses = c(rep("character",2), rep("numeric", 21))) %>%
   reduce(rbind) %>% 
   select(seq(1,22,1))
+
+rm(files)
   
 names(df) <- c("Date",	"Time",	"Weekday",	"P_pump",
            "p_NDIR",	"p_in",	"I_total",	"U_supply",
@@ -18,6 +22,26 @@ names(df) <- c("Date",	"Time",	"Weekday",	"P_pump",
           "xCO2_corr",	"T_control",	"T_gas",	"%rH_gas")
 
 df <- data.table(df)
+df$date <- ymd_hms(paste(df$Date, df$Time))
 
-ggplot(df[Zero==1], aes(Runtime, pCO2_corr))+
+zero <- data.table(
+  df[Zero==1] %>%
+  arrange(date) %>%   
+  mutate(Diff = Runtime - lag(Runtime)))
+
+zero[Diff > 11]
+
+
+ggplot(zero, aes(date, Diff))+
   geom_point()
+
+
+ggplot(df[Zero==1], aes(date, pCO2_corr))+
+  geom_point()+
+  ylim(0,70)
+
+
+
+
+
+
