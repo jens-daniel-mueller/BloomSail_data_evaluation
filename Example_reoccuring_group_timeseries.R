63ff26e2aeb2f742c73a180f74f73a0be1e8a3de
jens-daniel-mueller <jens.mueller@io-warnemuende.de>
2018-11-08 14:05:12 +0100

    Removed file

--- Example_reoccuring_group_timeseries.R
+++
@@ -1,9 +0,0 @@
-library(lubridate)
-
-df <- data.frame(
-    date = seq(ymd("2018-01-01"), ymd("2018-06-29"), by = "days"),
-    flag = rep( c(rep(1,10), rep(0, 20)), 6),
-    value = seq(1,180,1)
-  )
-
-df
