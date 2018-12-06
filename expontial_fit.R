68f2a4e23208b07eb566e088e848ce22eb3d362d
jens-daniel-mueller <jens.mueller@io-warnemuende.de>
2018-12-06 17:39:44 +0100

    tested various exponential fit procedures

--- expontial_fit.R
+++ expontial_fit.R
@@ -6,23 +6,27 @@ y1 = 22 + (53 - 22) * exp(-0.02 * t) %>% jitter(10)
 y2 = 24 + (60 - 22) * exp(-0.01 * t) %>% jitter(10)
 
 
-df <- data.frame(t = t, y = y1, sensor = 'sensor1') %>% 
+test <- data.frame(t = t, y = y1, sensor = 'sensor1') %>% 
   rbind(. , data.frame(t = t, y = y2, sensor = 'sensor2'))
 sensor1 <- df %>% filter(sensor == 'sensor1')
 
 
-qplot(t, y, data = df, colour = sensor)
+qplot(t, y, data = test, colour = sensor)
 
 
-nls(y ~ yf + (y0 - yf) * exp(-alpha * t), 
-    data = sensor1,
-    start = list(y0 = 54, yf = 25, alpha = 1))
+# nls(y ~ yf + (y0 - yf) * exp(-alpha * t), 
+#     data = sensor1,
+#     start = list(y0 = 54, yf = 25, alpha = 1))
 
 fit <- nls(y ~ SSasymp(t, yf, y0, log_alpha), data = sensor1)
 fit
 
 
 qplot(t, y, data = augment(fit)) + geom_line(aes(y = .fitted))
+
+
+
+
 
 
 df %>% 
