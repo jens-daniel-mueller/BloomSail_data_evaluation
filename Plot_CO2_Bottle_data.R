43b7d82cf4b1208a72a23748e7825cc75a692964
jens-daniel-mueller <jens.mueller@io-warnemuende.de>
2019-01-04 14:26:49 +0100

    direct plot of bottle data

--- Plot_CO2_Bottle_data.R
+++ Plot_CO2_Bottle_data.R
@@ -14,6 +14,16 @@ Sensor$start.date <- ymd_hms(Sensor$start.date)
 setwd("C:/Mueller_Jens_Data/180530_BloomSail/Data/Bottle/CO2")
 Bottle <- data.table(read.csv("BloomSail_bottle_CO2_all.csv"))
 
+# Bottle$date <- ymd(Bottle$transect.ID)
+# 
+# Bottle$CT <- as.numeric(as.character(Bottle$CT))
+# 
+# Bottle %>% 
+#   filter(label %in% c("P07", "P10")) %>% 
+#   ggplot()+
+#   geom_path(aes(date, CT, col=as.factor(Dep)))+
+#   facet_wrap(~label)
+
 
 stations <-
   Sensor %>% 
