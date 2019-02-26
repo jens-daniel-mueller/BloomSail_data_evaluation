c287addeb1bce03247b337629ab6d74a0c9bf4f0
jens-daniel-mueller <jens.mueller@io-warnemuende.de>
2019-02-26 15:53:53 +0100

    Read-in and preliminary plot

--- 1_Read_Tina_V_Bottle_Nutrients.R
+++ 1_Read_Tina_V_Bottle_Nutrients.R
@@ -6,33 +6,30 @@ library(here)
 
 
 #### read data set from relative path ####
-df <- read_csv(here("Data/Bottle/Gases", "BloomSail_bottle_tracegases_all.csv"))
+df <- read_csv(here("Data/Tina_V/Bottle/Nutrients", "nut_jens_JM.csv"))
 
-
-
-
-stations <-
-  Sensor %>% 
-  filter(label %in% unique(Bottle$label)) %>% 
-  group_by(transect.ID, label) %>% 
-  summarise(date = mean(date),
-            lat = mean(lat),
-            lon = mean(lon)) %>% 
-  ungroup()
-
-
-df <- merge(Bottle, stations, all=TRUE)
-df$CT <- as.numeric(as.character(df$CT))
-df <- na.omit(df)
-
+df <-
 df %>% 
-  filter(label %in% c("P07", "P10")) %>% 
-  ggplot()+
-    geom_path(aes(date, CT, col=as.factor(Dep)))+
-    facet_wrap(~label)
-  
+  mutate(date = substr(datelabel, 1, 6),
+         label = substr(datelabel, 7, 9),
+         datelabel = NULL,
+         date = ymd(date))
 
 
+df.long <-
+  df %>% 
+  gather("parameter", "value", 2:6)
 
 
+df.long %>% 
+  filter(label %in% c("P07", "P10"),
+         parameter %in% c("NO23", "PO4")) %>% 
+  ggplot(aes(date, value, fill=dep))+
+  geom_point(shape=21)+
+  scale_fill_viridis_c(name = "Depth (m)")+
+  labs(x="Date", y="Concentration (µM)")+
+  facet_grid(parameter~label, scales = "free")+
+  theme_bw()
+
+ggsave(here("Plots/Tina_V/Bottle/Nutrients", "BloomSail_Nuts_P07-P10.jpg"))
 
