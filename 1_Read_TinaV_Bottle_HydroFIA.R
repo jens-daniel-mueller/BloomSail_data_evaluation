2f7fcc4da72054c198875e61cce9cfea740153ed
jens-daniel-mueller <jens.mueller@io-warnemuende.de>
2019-03-18 14:20:06 +0100

    update data read-in

--- 1_Read_TinaV_Bottle_HydroFIA.R
+++ 1_Read_TinaV_Bottle_HydroFIA.R
@@ -1,38 +1,55 @@
 library(data.table)
 library(lubridate)
 library(tidyverse)
+library(here)
 
 
-####
+#### Read and transform HydroFIA pH dataset ####
 
-setwd("C:/Mueller_Jens_Data/180530_BloomSail/BloomSail_data_evaluation/Data/Tina_V/Bottle/HydroFIA_pH")
 
-HF <- data.table(read.delim("180824_data.txt", sep=",", skip = 2))[,c(1,3,6,7,8,9)]
-names(HF) <- c("date", "sample", "Sal", "pH.Mosley.25", "pH.fitpoints", "pH.error")
+HF <- read_csv(here("Data/TinaV/Bottle/pH_HydroFIA", "2019-02-26-14-05-14-pH-recalculated.csv")) %>% 
+  mutate(ID = str_sub(sampleName, 1,6),
+         station = str_sub(sampleName, 8,10),
+         dep = as.numeric(str_sub(sampleName, 12,13))) %>% 
+  select(date_time_meas=timeStamp, ID, station, dep,
+         salinity, tem_meas=temperature, pHT=ph_mueller, pHT_error=ph_mueller_error) %>% 
+  filter(!is.na(dep))
 
-HF$date <- ymd_hms(HF$date)
-HF <- HF[date > ymd("18-07-01")]
 
-HF$transect.ID <- substr(HF$sample, 1,6)
-HF$type <- substr(HF$sample, 8,8)
-HF$label <- substr(HF$sample, 8,10)
-HF$Dep.HF <- as.numeric(substr(HF$sample, 12,13))
+#### Check data quality ####
 
-HF <- HF[label %in% c("P07", "P10")]
-HF <- HF[, .SD[seq(.N-2,.N)], by=.(transect.ID, label, Dep.HF)]
+HF %>% 
+  filter(station  %in% c("P07", "P10")) %>% 
+  ggplot(aes(date_time_meas, pHT))+
+  geom_path()+
+  geom_point()+
+  scale_y_continuous(breaks = seq(5,10,0.005))+
+  facet_wrap(~interaction(ID,station,dep), scales = "free")
 
-ggplot(data = HF, 
-       aes(pH.Mosley.25, Dep.HF, 
-           col=transect.ID))+
+HF <- HF %>% 
+  group_by(ID,station,dep) %>% 
+  slice(tail(row_number(), 3)) %>%  
+  ungroup()
+
+
+HF %>% 
+  filter(station  %in% c("P07", "P10")) %>% 
+  ggplot(aes(pHT, dep, col=ID))+
   geom_path()+
   geom_point()+
   scale_y_reverse()+
-  facet_wrap(~label)+
-  #xlim(0,27)+
-  #scale_color_viridis(discrete = TRUE, direction = -1)+
+  facet_wrap(~station)+
   scale_color_brewer(palette = "Spectral")+
-  labs(x="pH [spec, Mosley, 25C]", y="Depth [m]")
+  labs(x="pH [spec, Mueller, 25C]", y="Depth [m]")
+
+
+# HF %>% 
+#   filter(station  %in% c("P07", "P10")) %>%   
+#   ggplot(aes(date.time, pHT, col=as.factor(dep)))+
+#   geom_path()+
+#   geom_point()+
+#   facet_wrap(~station)
 
 
 
-
+write_csv(HF, here("Data/_summarized_data_files", "TinaV_bottle_pH_HydroFIA.csv"))
