ca017cd706cde2a6b71c6b5574fc44b243cc1f9f
jens-daniel-mueller <jens.mueller@io-warnemuende.de>
2019-03-25 09:15:07 +0100

    CT calculation

--- 3_Calculate_seawater_CT_TinaV_Sensor.R
+++ 3_Calculate_seawater_CT_TinaV_Sensor.R
@@ -14,23 +14,43 @@ Sensor <- read_csv(here::here("Data/_merged_data_files", "BloomSail_Sensor_Hydro
 #### * in CT indicates that absolute values are somewhat vague, but changes in CT are represented well
 #### underlying pCO2 data still require Response time (RT) correction
 
+Sensor %>% 
+  filter(is.na(Flush))
 
-#Sensor[transect.ID == "180616"]$pCO2 <- NA
 
-# Sensor$CT <- 0
-# Sensor$CT <- NA
 
-Sensor <- Sensor %>%
+
+Sensor_CT <- Sensor %>%
   filter(!is.na(pCO2),
          !is.na(sal),
          !is.na(tem),
-         !is.na(dep),
-         pCO2 >30) %>%
+         !is.na(Flush),
+         !is.na(Zero),
+         Zero != 1,
+         Flush !=1,
+         pCO2 > 60) %>%
   mutate(CT = carb(24, var1=pCO2, var2=1720*1e-6,
                    S=sal, T=tem, P=dep/10, k1k2="m10", kf="dg", ks="d",
                    pHscale="T", gas="insitu")[,16]*1e6)
 
-Sensor %>% 
+
+
+
+Sensor_CT %>% 
   filter(dep<5, dep>2) %>% 
-  ggplot(aes(date_time, CT, col=Flush)) +
+  ggplot(aes(date_time, CT, col=type)) +
   geom_point()
+
+
+df <- full_join(Sensor, Sensor_CT)
+
+df %>% 
+  filter(dep<5, dep>2, station=="P07") %>% 
+  ggplot(aes(O2, pH, col=ID))+
+  geom_point()+
+  geom_path()+
+  scale_color_viridis_c()
+
+
+write_csv(df, here::here("Data/_merged_data_files", "BloomSail_Sensor_HydroC_CT.csv"))
+
