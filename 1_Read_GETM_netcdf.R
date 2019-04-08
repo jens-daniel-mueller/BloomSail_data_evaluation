500f53e366ed74458d8082ef511bfa14f7b0dbff
jens-daniel-mueller <jens.mueller@io-warnemuende.de>
2019-04-08 16:36:05 +0200

    included 2018 data

--- 1_Read_GETM_netcdf.R
+++ 1_Read_GETM_netcdf.R
@@ -5,13 +5,25 @@ library(ncdf4)
 library(raster)
 library(here)
 library(geosphere)
+library(lubridate)
 
 
 #### Plots section data below Finnmaid track ####
 
-#### read ncf file ####
+#### read lon from old ncf file ####
 
 E_sec <- nc_open(here::here("Data/GETM", "Finnmaid.E.TSage.zax.nc"))
+print(E_sec)
+
+lon <- ncvar_get(E_sec, "lonc")
+
+nc_close(E_sec) 
+rm(E_sec) 
+
+
+#### read ncf file ####
+
+E_sec <- nc_open(here::here("Data/GETM", "2018_Finnmaid.E.TSage.zax.nc"))
 print(E_sec)
 
 lon <- ncvar_get(E_sec, "lonc")
@@ -37,12 +49,15 @@ E_sec_array[E_sec_array == fillvalue$value] <- NA
 
 max_col <- max(E_sec_array, na.rm = TRUE)
 min_col <- min(E_sec_array, na.rm = TRUE)
+
 # i <- 3
 for (i in seq(1,length(t),1)){
 
 E_sec_slice <- E_sec_array[, , i] 
 E_sec_r <- raster(t(E_sec_slice), xmn=min(lon), xmx=max(lon), ymn=min(zax), ymx=max(zax))#,
 E_sec_r <- flip(E_sec_r, direction='y')
+
+date <- ymd_hms("2018-4-1 00:00:00")+t[i]
 
 #### Plotting #####
 E_sec_r_df <- as.data.frame(E_sec_r, xy=TRUE)
@@ -51,14 +66,14 @@ E_sec_r_df %>%
 ggplot() +
   geom_raster(aes(x = x, y = -y, 
                   fill = layer)) + 
-  scale_fill_viridis_c(name="SST (°C)",
-                       limits = c(min_col,max_col))+
+  scale_fill_viridis_c(name="SST (°C)")+#,
+                       #limits = c(min_col,max_col))+
   scale_y_reverse()+
-  labs(x="Lon (°E)", y="Depth (m)", title = paste("August",i ,"| Route: E"))+
+  labs(x="Lon (°E)", y="Depth (m)", title = paste(date ,"| Route: E"))+
   coord_cartesian(expand = 0)+
   theme_bw()
 
-ggsave(here::here("Plots/GETM/Sections", paste(i,"_August_2017_Temp_Route_E.jpg",sep = "")),
+ggsave(here::here("Plots/GETM/Sections", paste(as.character(date),"_Temp_Route_E.jpg",sep = "")),
        width = 7, height = 4, dpi = 150)
 
 rm(E_sec_slice, E_sec_r, E_sec_r_df)
@@ -67,23 +82,25 @@ rm(E_sec_slice, E_sec_r, E_sec_r_df)
 
 
 
+
 #### Plots timeseries of water column parameters (mixing depth, SST ...) vs location ####
 
 #### Read and plot timeseries data ####
 
-E_ts <- nc_open(here::here("Data/GETM", "Finnmaid.E.2d.nc"))
+E_ts <- nc_open(here::here("Data/GETM", "2018_Finnmaid.E.2d.nc"))
 print(E_ts)
 
-#lon <- ncvar_get(E_ts, "lonc")
+lon <- ncvar_get(E_ts, "lonc")
 lat <- ncvar_get(E_ts, "latc", verbose = F)
-t <- ncvar_get(E_ts, "time")/(60*60*24)
-#SST <- ncvar_get(E_ts, "SST")
+t <- ymd_hms("2018-4-1 00:00:00")+ncvar_get(E_ts, "time")
+#t <- ncvar_get(E_ts, "time")/(60*60*24)
+SST <- ncvar_get(E_ts, "SST")
 
 head(t) # look at the first few entries in the time vector
 
 #var <- "mld_age_1"
 
-for (var in names(E_ts$var)){
+for (var in c("SSS","SST","mld_age_1","mld_age_3","mld_age_5", "mld_rho","mld_tke")){
 
 E_ts_array <- ncvar_get(E_ts, var) # store the data in a 3-dimensional array
 #dim(E_ts_array) # should be 2d with dimensions: 1575 coordinate, 31d*(24h/d/3h)=248 time steps
@@ -100,16 +117,21 @@ E_ts_array_r <- flip(E_ts_array_r, direction='y')
 
 #### Plotting #####
 E_ts_array_r_df <- as.data.frame(E_ts_array_r, xy=TRUE)
+E_ts_array_r_df <- E_ts_array_r_df %>% 
+  arrange(x,y) %>% 
+  mutate(y = rep(t, length(lon)))
 
 E_ts_array_r_df %>% 
+  filter(layer <= 30,
+         layer >= 0) %>% 
   ggplot() +
   geom_raster(aes(y, x, fill = layer)) + 
-  scale_fill_viridis_c(name=var)+
-  labs(x="Days", y="Lon (°E)", title = paste("August 2017","| Finnmaid Route: E"))+
+  scale_fill_viridis_c(name=var, direction = -1)+
+  labs(x="Date", y="Lat (°N)", title = paste("2018 | Finnmaid Route: E"))+
   theme_bw()+
   coord_cartesian(expand = 0)
 
-ggsave(here::here("Plots/GETM/timeseries", paste(var,"_E_Aug_2017.jpg",sep = "")),
+ggsave(here::here("Plots/GETM/timeseries", paste(var,"_E_2018.jpg",sep = "")),
        width = 7, height = 5, dpi = 300)
 
 rm(E_ts_array, E_ts_array_r, E_ts_array_r_df, fillvalue)
