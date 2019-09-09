8051be483a3c9086927cafb665bd0b6b5aef4842
jens-daniel-mueller <jens.mueller@io-warnemuende.de>
2019-09-09 18:34:12 +0200

    minor changes

--- 2_Response_Time_determination_TinaV_Sensor_HydroC.R
+++ 2_Response_Time_determination_TinaV_Sensor_HydroC.R
@@ -4,16 +4,14 @@ library(tidyverse)
 library(broom)
 
 
-# Determine RT by exponential fit -----------------------------------------
 # inspired by http://douglas-watson.github.io/post/2018-09_exponential_curve_fitting/
-
 
 # Read and prepare data ---------------------------------------------------
 
 df <- read_csv(here::here("Data/_summarized_data_files",
                           "Tina_V_Sensor_HydroC_Flush.csv"))
 
-df <- df %>% 
+df <- df %>%
   filter(FlushZeroID != 53)
 
 df <- df %>% 
@@ -23,26 +21,25 @@ df <- df %>%
 
 # Plot individual Flush periods with exponential fit ----------------------
 
-# for (i in unique(df$FlushZeroID)) {
-# 
-# df_ID <- df %>% 
-#   filter(FlushZeroID == i)
-# 
-# fit <- df_ID %>% 
-#   filter(mixing == "equilibration") %>% 
-#   nls(pCO2_corr ~ SSasymp(duration_equi, yf, y0, log_alpha), data = .)
-# 
-# tau <- as.numeric(exp(-tidy(fit)[3,2]))
-# 
-# qplot(duration_equi, pCO2_corr, data = augment(fit)) +
-#   geom_line(aes(y = .fitted))+
-#   geom_vline(xintercept = tau)
-# 
-# ggsave(here::here("/Plots/TinaV/Sensor/HydroC_diagnostics/Response_time_fits",
-#                   paste(i,"_FlushZeroID_HydroC_RT.jpg", sep="")),
-#          width = 10, height = 4)
-# }
+for (i in unique(df$FlushZeroID)) {
 
+df_ID <- df %>%
+  filter(FlushZeroID == i)
+
+fit <- df_ID %>%
+  filter(mixing == "equilibration") %>%
+  nls(pCO2_corr ~ SSasymp(duration_equi, yf, y0, log_alpha), data = .)
+
+tau <- as.numeric(exp(-tidy(fit)[3,2]))
+
+qplot(duration_equi, pCO2_corr, data = augment(fit)) +
+  geom_line(aes(y = .fitted))+
+  geom_vline(xintercept = tau)
+
+ggsave(here::here("/Plots/TinaV/Sensor/HydroC_diagnostics/Response_time_fits",
+                  paste(i,"_FlushZeroID_HydroC_RT.jpg", sep="")),
+         width = 10, height = 4)
+}
 
 
 # Determine response times ------------------------------------------------
@@ -58,7 +55,6 @@ RT <- df %>%
   mutate(tau = exp(-log_alpha))
 
 
-
 # Determine mean absolute residual from fit -------------------------------
 
 augmented <- df %>% 
@@ -67,11 +63,10 @@ augmented <- df %>%
   do(fit = nls(pCO2_corr ~ SSasymp(duration_equi, yf, y0, log_alpha), data = .)) %>% 
   augment(fit)
 
-
-qplot(duration_equi, pCO2_corr, data = augmented, geom = 'point', colour = as.factor(FlushZeroID)) +
-  geom_line(aes(y=.fitted))
-
-qplot(duration_equi, .resid, data = augmented, geom = 'point', colour = as.factor(FlushZeroID))
+# qplot(duration_equi, pCO2_corr, data = augmented, geom = 'point', colour = as.factor(FlushZeroID)) +
+#   geom_line(aes(y=.fitted))
+# 
+# qplot(duration_equi, .resid, data = augmented, geom = 'point', colour = as.factor(FlushZeroID))
 
 augmented_sum <- augmented %>% 
   group_by(FlushZeroID) %>% 
@@ -90,19 +85,23 @@ St_Err <- df %>%
   select(1,2) %>% 
   rename(tau_st_err = log_alpha)
 
-
-
 # Merge RT, mean residuals and St error -----------------------------------
 
 RT <- full_join(RT, augmented_sum)
 RT <- full_join(RT, St_Err)
 
 
-
 # Plot RTs over fielt trip period -----------------------------------------
 
 RT %>% 
   filter(mean_resid < 0.4) %>% 
+  ggplot(aes(FlushZeroID, tau, col=mean_resid, label=FlushZeroID)) +
+  geom_point()+
+  scale_color_viridis_c()+
+  geom_label()
+
+RT %>% 
+  filter(mean_resid < 1) %>% 
   ggplot(aes(FlushZeroID, tau, col=mean_resid)) +
   geom_point()+
   scale_color_viridis_c()
