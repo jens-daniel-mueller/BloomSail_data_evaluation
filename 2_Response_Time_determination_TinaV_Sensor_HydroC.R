4e37b6a33f5afe110a63b623e1392456ce77be17
jens-daniel-mueller <jens.mueller@io-warnemuende.de>
2019-09-11 10:28:42 +0200

    Relative residual from fit and threshold selection

--- 2_Response_Time_determination_TinaV_Sensor_HydroC.R
+++ 2_Response_Time_determination_TinaV_Sensor_HydroC.R
@@ -2,6 +2,7 @@
 
 library(tidyverse)
 library(broom)
+library(lubridate)
 
 
 # inspired by http://douglas-watson.github.io/post/2018-09_exponential_curve_fitting/
@@ -21,28 +22,31 @@ df <- df %>%
 
 # Plot individual Flush periods with exponential fit ----------------------
 
-for (i in unique(df$FlushZeroID)) {
+# for (i in unique(df$FlushZeroID)) {
+# 
+# df_ID <- df %>%
+#   filter(FlushZeroID == i)
+# 
+# fit <- df_ID %>%
+#   filter(mixing == "equilibration") %>%
+#   nls(pCO2_corr ~ SSasymp(duration_equi, yf, y0, log_alpha), data = .)
+# 
+# tau <- as.numeric(exp(-tidy(fit)[3,2]))
+# #RSS <- sum(resid(fit)^2)
+# 
+# augment(fit) %>% 
+#   ggplot(aes(duration_equi, pCO2_corr))+
+#   geom_point()+
+#   geom_line(aes(y = .fitted))+
+#   geom_vline(xintercept = tau)
+# 
+# ggsave(here::here("/Plots/TinaV/Sensor/HydroC_diagnostics/Response_time_fits",
+#                   paste(i,"_FlushZeroID_HydroC_RT.jpg", sep="")),
+#          width = 10, height = 4)
+# }
 
-df_ID <- df %>%
-  filter(FlushZeroID == i)
 
-fit <- df_ID %>%
-  filter(mixing == "equilibration") %>%
-  nls(pCO2_corr ~ SSasymp(duration_equi, yf, y0, log_alpha), data = .)
-
-tau <- as.numeric(exp(-tidy(fit)[3,2]))
-
-qplot(duration_equi, pCO2_corr, data = augment(fit)) +
-  geom_line(aes(y = .fitted))+
-  geom_vline(xintercept = tau)
-
-ggsave(here::here("/Plots/TinaV/Sensor/HydroC_diagnostics/Response_time_fits",
-                  paste(i,"_FlushZeroID_HydroC_RT.jpg", sep="")),
-         width = 10, height = 4)
-}
-
-
-# Determine response times ------------------------------------------------
+# Response time fitting ---------------------------------------------------
 
 RT <- df %>% 
   filter(mixing == "equilibration") %>% 
@@ -55,7 +59,7 @@ RT <- df %>%
   mutate(tau = exp(-log_alpha))
 
 
-# Determine mean absolute residual from fit -------------------------------
+# Residuals from fit ------------------------------------------------------
 
 augmented <- df %>% 
   filter(mixing == "equilibration") %>% 
@@ -70,10 +74,13 @@ augmented <- df %>%
 
 augmented_sum <- augmented %>% 
   group_by(FlushZeroID) %>% 
-  summarise(mean_resid = mean(abs(.resid)))
+  summarise(mean_resid = mean(abs(.resid)),
+            mean_resid_rel = mean(abs(.resid))/max(pCO2_corr),
+            max_pCO2_corr = max(pCO2_corr))
 
 
-# Determine standard error of tau -----------------------------------------
+
+# Standard error of tau ---------------------------------------------------
 
 St_Err <- df %>% 
   filter(mixing == "equilibration") %>% 
@@ -85,42 +92,49 @@ St_Err <- df %>%
   select(1,2) %>% 
   rename(tau_st_err = log_alpha)
 
+
 # Merge RT, mean residuals and St error -----------------------------------
 
 RT <- full_join(RT, augmented_sum)
 RT <- full_join(RT, St_Err)
 
+rm(augmented, augmented_sum, St_Err)
 
-# Plot RTs over fielt trip period -----------------------------------------
+
+
+# Identify residual threshold ---------------------------------------------
 
 RT %>% 
-  filter(mean_resid < 0.4) %>% 
-  ggplot(aes(FlushZeroID, tau, col=mean_resid, label=FlushZeroID)) +
-  geom_point()+
-  scale_color_viridis_c()+
+  ggplot(aes(mean_resid_rel, FlushZeroID, label=FlushZeroID)) +
+  geom_point(shape=21)+
+  scale_fill_viridis_c()+
   geom_label()
 
 RT %>% 
-  filter(mean_resid < 1) %>% 
-  ggplot(aes(FlushZeroID, tau, col=mean_resid)) +
-  geom_point()+
-  scale_color_viridis_c()
+  filter(mean_resid_rel >= 0.0065)
 
 RT %>% 
-  filter(tau_st_err < 0.025) %>% 
-  ggplot(aes(FlushZeroID, tau, col=tau_st_err)) +
-  geom_point()+
-  scale_color_viridis_c()
+  filter(mean_resid_rel < 0.0065) %>% 
+  ggplot(aes(FlushZeroID, tau, label=round(mean_resid_rel,4))) +
+  geom_label(data=RT, aes(FlushZeroID, tau, label=round(mean_resid_rel,4)), shape=21, col="red") +
+  geom_point(shape=21)+
+  scale_fill_viridis_c()+
+  geom_label()
+
+
+
+# Mean tau ----------------------------------------------------------------
+
+max(unique(df[df$date_time < ymd_hms("2018-07-17;13:08:34"),]$FlushZeroID))
+unique(df[df$date_time > ymd_hms("2018-07-17;13:08:34"),]$FlushZeroID)
+
 
 RT %>% 
-  #filter(tau < 100) %>% 
-  ggplot(aes(tau, tau_st_err)) +
-  geom_point()
+  filter(mean_resid_rel < 0.0065) %>% 
+  mutate(pump_power = if_else(FlushZeroID <= 20, "1W", "8W")) %>% 
+  group_by(pump_power) %>% 
+  summarise(tau = mean(tau))
 
-RT %>% 
-  #filter(tau < 100) %>% 
-  ggplot(aes(mean_resid, tau_st_err)) +
-  geom_point()
 
 
 
