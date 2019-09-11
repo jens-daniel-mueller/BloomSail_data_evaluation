9842550cb8a07d404146e90ec877f3fa27e7566e
jens-daniel-mueller <jens.mueller@io-warnemuende.de>
2019-09-11 17:38:46 +0200

    Proper Zero and Flush labeling

--- 2_Response_Time_correction_TinaV_Sensor_HydroC.R
+++ 2_Response_Time_correction_TinaV_Sensor_HydroC.R
@@ -7,96 +7,34 @@ library(tibbletime)
 
 # Load Contros corrected data set -----------------------------------------
 
-HC <- read_csv(here::here("Data/_summarized_data_files", "Tina_V_Sensor_HydroC.csv"))
-
-# Identify deployments and remove irrelevant data -------------------------
-
-# HC <- HC %>% 
-#   mutate(deployment = cumsum(c(TRUE,diff(date_time)>=30)),
-#          week = week(date_time))
-# 
-# HC_test <- HC %>% 
-#   filter(Flush == 1) %>% 
-#   mutate(deployment = cumsum(c(TRUE,diff(date_time)>=30)))
-# 
-# HC.test %>% 
-#   ggplot(aes(date_time, pCO2_corr))+
-#   geom_point()+
-#   facet_wrap(~deployment, scales = "free")
-
-#i <- unique(HC$week)[1]
-# for (i in unique(HC$week)) {
-#  
-#   HC %>% 
-#     filter(week(date_time) == i) %>%
-#     ggplot(aes(date_time, pCO2_corr, col=as.factor(deployment)))+
-#     geom_line()
-#   
-#   ggsave(here::here("/Plots/TinaV/Sensor/HydroC_diagnostics", paste(i,"_HydroC_weekly_timeseries.jpg", sep="")),
-#          width = 8, height = 4)
-#    
-# } 
-
-# HC %>% 
-#   ggplot(aes(date_time, pCO2_corr, col=as.factor(deployment)))+
-#   geom_line()
-# 
-# ggsave(here::here("/Plots/TinaV/Sensor/HydroC_diagnostics", "HydroC_timeseries.jpg"),
-#        width = 40, height = 4)
-
-HC %>% 
-  filter(deployment == 29) %>%
-  ggplot(aes(date_time, pCO2_corr, col=as.factor(deployment)))+
-  geom_line()
-
-HC_RT_study <- HC %>% 
-  filter(deployment == 29)
-
-HC <- HC %>% 
-  filter(deployment %in% c(2,6,9,14,17,21,23,27,31,33,34,35,37))
-
-# for (i in unique(HC_clean$week)) {
-#   
-#   HC_clean %>% 
-#     filter(week(date_time) == i) %>%
-#     ggplot(aes(date_time, pCO2_corr, col=as.factor(deployment)))+
-#     geom_line()
-#   
-#   ggsave(here::here("/Plots/TinaV/Sensor/HydroC_diagnostics", paste(i,"_HydroC_weekly_timeseries_clean.jpg", sep="")),
-#          width = 8, height = 4)
-#   
-# } 
-
-# HC_clean %>%
-#   ggplot(aes(date_time, pCO2_corr, col=as.factor(deployment)))+
-#   geom_line()
-# 
-# ggsave(here::here("/Plots/TinaV/Sensor/HydroC_diagnostics", "HydroC_timeseries_clean.jpg"),
-#        width = 40, height = 4)
+df <- read_csv(here::here("Data/_summarized_data_files", "Tina_V_Sensor_HydroC.csv"))
 
 
-
-# Define response time correction approach after Bittig -------------------
+# Response time correction approach after Bittig --------------------------
 
 RT_corr <- function(c1, c0, dt, tau) {
   ( 1 / ( 2* (( 1+(2*tau/dt) )^(-1) ))) * (c1 - (1-(2* (( 1+(2*tau/dt) )^(-1) ))) * c0)
 }
 
 
-
-# Apply RT correction -----------------------------------------------------
-
+# Response time (tau) values ----------------------------------------------
 
 tau_low <- 79.9
 tau_high <- 55.9
 
 
-HC <- HC %>% 
+# Prepare data set for RT correction --------------------------------------
+
+df_full <- df %>% 
+  select(date_time, deployment, Zero_ID, Zero, Flush, pCO2 = pCO2_corr)
+
+df <- df %>% 
+  filter(Zero == 0, Flush == 0) %>% 
+  select(date_time, deployment, Zero_ID, Zero, Flush, pCO2 = pCO2_corr) %>% 
   arrange(date_time) %>% 
-  select(date_time, Zero, Flush, deployment, pCO2 = pCO2_corr) %>% 
   mutate(dt = as.numeric(as.character(date_time - lag(date_time))),
          tau = if_else(date_time < ymd_hms("2018-07-17;13:08:34"),
-                       80, 55))
+                       tau_low, tau_high))
 
 
 # Apply RT correction to entire data set ----------------------------------
@@ -106,7 +44,8 @@ window <- 15
 rolling_mean   <- rollify(~mean(.x, na.rm = TRUE), window = window)
 rolling_median <- rollify(~median(.x, na.rm = TRUE), window = window)
 
-HC <- HC %>%
+
+df <- df %>%
   group_by(deployment) %>% 
   mutate(pCO2_RT = RT_corr(pCO2, lag(pCO2), dt, tau),
          pCO2_RT = if_else(pCO2_RT %in% c(Inf, -Inf), NaN, pCO2_RT),
@@ -116,64 +55,80 @@ HC <- HC %>%
 
 # time shift RT corrected data
 shift <- as.integer(as.character(window/2))
-HC <- HC %>%
+df <- df %>%
   mutate(pCO2_RT_mean = lead(pCO2_RT_mean, shift),
          pCO2_RT_median = lead(pCO2_RT_median, shift))
 
-rm(rolling_median, rolling_mean, shift)
-      
+rm(rolling_median, rolling_mean, shift, tau_high, tau_low, window, RT_corr)
+     
+df <- full_join(df_full, df)
+rm(df_full)
 
-unique(HC$deployment)
+# Plot RT corrected data by deployment and Zero_ID ------------------------
 
-HC %>% 
-  filter(pCO2_RT_mean == Inf)
-
-HC %>% 
-  filter(deployment == unique(HC$deployment)[8]) %>% 
-  ggplot()+
-  geom_line(aes(date_time, pCO2, col="Raw"))+
-  geom_line(aes(date_time, pCO2_RT_median, col="median"))+
-  geom_line(aes(date_time, pCO2_RT_mean, col="mean"))
-
+for (depID in unique(df$deployment)) {
+  
+  df_dep <- df %>%
+    filter(deployment == depID)
+  
+  for (zerID in unique(df_dep$Zero_ID)) {
+    
+    df_dep %>%
+      filter(Zero_ID == zerID) %>% 
+      ggplot()+
+      geom_line(aes(date_time, pCO2, col="Raw"))+
+      geom_line(aes(date_time, pCO2_RT_median, col="median"))+
+      geom_line(aes(date_time, pCO2_RT_mean, col="mean"))
+    
+    ggsave(here::here("/Plots/TinaV/Sensor/HydroC_diagnostics/Response_time_corection",
+                      paste(depID,"_deployment_",zerID,"_Zero_ID_HydroC_pCO2_RTcorr.jpg", sep="")),
+           width = 10, height = 4)
+    
+  }
+}
+    
 
 # Write RT corrected data to file -----------------------------------------
 
-write_csv(HC, here::here("Data/_summarized_data_files",
+write_csv(df, here::here("Data/_summarized_data_files",
                              "Tina_V_Sensor_HydroC_RTcorr.csv"))    
     
     
 
 
 
-
+# X -----------------------------------------------------------------------
+# X -----------------------------------------------------------------------
 # ___old part: Testing for restricted time intervals ----------------------
+# X -----------------------------------------------------------------------
+# X -----------------------------------------------------------------------
 
 
 # Subset test data with high and low measurement frequency ----------------
 
-HC %>% 
+df %>% 
   filter(date_time > ymd_hm("2018-07-24T1800"),
          date_time < ymd_hm("2018-07-25T0000")) %>%
   ggplot(aes(date_time, pCO2_corr))+
   geom_line()
 
-HC %>% 
+df %>% 
   filter(date_time > ymd_hm("2018-07-10T0400"),
          date_time < ymd_hm("2018-07-10T1700")) %>%
   ggplot(aes(date_time, pCO2_corr))+
   geom_line()
 
-high_freq <- HC %>% 
+high_freq <- df %>% 
   filter(date_time > ymd_hm("2018-07-24T1800"),
          date_time < ymd_hm("2018-07-25T0000")) %>% 
   mutate(freq = "high")
 
-low_freq <- HC %>% 
+low_freq <- df %>% 
   filter(date_time > ymd_hm("2018-07-10T0400"),
          date_time < ymd_hm("2018-07-10T1700")) %>%
   mutate(freq = "low")
 
-HC <- bind_rows(low_freq, high_freq) %>% 
+df <- bind_rows(low_freq, high_freq) %>% 
   select(freq, date_time, pCO2 = pCO2_corr)
 
 rm(low_freq, high_freq)
@@ -190,7 +145,7 @@ RT_corr <- function(c1, c0, dt, tau) {
 
 # Apply RT correction -----------------------------------------------------
 
-HC <- HC %>% 
+df <- df %>% 
   arrange(date_time) %>% 
   mutate(dt = as.numeric(as.character(date_time - lag(date_time))))
 
@@ -201,7 +156,7 @@ for (freq in c("high", "low")) {
   rolling_mean   <- rollify(mean, window = window)
   rolling_median <- rollify(median, window = window)
     
-  temp <- HC %>%
+  temp <- df %>%
     filter(freq == freq) %>% 
     mutate(tau = tau,
            pCO2_RT = RT_corr(pCO2, lag(pCO2), dt, tau),
@@ -215,9 +170,9 @@ for (freq in c("high", "low")) {
     mutate(pCO2_RT_mean = lead(pCO2_RT_mean, shift),
            pCO2_RT_median = lead(pCO2_RT_median, shift))
    
-  if (exists("HC_RT", inherits = FALSE)){
-    HC_RT <- bind_rows(HC_RT, temp)
-  } else{HC_RT <- temp}
+  if (exists("df_RT", inherits = FALSE)){
+    df_RT <- bind_rows(df_RT, temp)
+  } else{df_RT <- temp}
   
   rm(rolling_median, rolling_mean, temp, shift)
   
@@ -226,7 +181,7 @@ for (freq in c("high", "low")) {
 }
 
 
-HC_RT %>% 
+df_RT %>% 
   filter(date_time > ymd_hm("2018-07-24T2220"),
          date_time < ymd_hm("2018-07-24T2250"),
          pCO2_RT > 0) %>%
@@ -243,7 +198,7 @@ HC_RT %>%
 ggsave(here::here("/Plots/TinaV/Sensor/HydroC_diagnostics", "HydroC_RTcorr_Bittig_2018-07-24_shallow.pdf"),
        width = 18, height = 12)
 
-HC_RT %>% 
+df_RT %>% 
   filter(date_time > ymd_hm("2018-07-24T2020"),
          date_time < ymd_hm("2018-07-24T2110"),
          pCO2_RT > 0) %>%
@@ -260,7 +215,7 @@ HC_RT %>%
 ggsave(here::here("/Plots/TinaV/Sensor/HydroC_diagnostics", "HydroC_RTcorr_Bittig_2018-07-24_deep.pdf"),
        width = 18, height = 12)
 
-HC_RT %>% 
+df_RT %>% 
   filter(date_time > ymd_hm("2018-07-10T0900"),
          date_time < ymd_hm("2018-07-10T1100"),
          pCO2_RT > 0) %>%
@@ -277,7 +232,7 @@ HC_RT %>%
 ggsave(here::here("/Plots/TinaV/Sensor/HydroC_diagnostics", "HydroC_RTcorr_Bittig_2018-07-10_deep.pdf"),
        width = 18, height = 12)
 
-HC_RT %>% 
+df_RT %>% 
   filter(date_time > ymd_hm("2018-07-10T1600"),
          date_time < ymd_hm("2018-07-10T1640"),
          pCO2_RT > 0) %>%
@@ -295,6 +250,6 @@ ggsave(here::here("/Plots/TinaV/Sensor/HydroC_diagnostics", "HydroC_RTcorr_Bitti
        width = 18, height = 12)
 
 
-write_csv(HC, here::here("Data/_summarized_data_files",
+write_csv(df, here::here("Data/_summarized_data_files",
                      "Tina_V_Sensor_HydroC_RTcorr.csv"))
 
