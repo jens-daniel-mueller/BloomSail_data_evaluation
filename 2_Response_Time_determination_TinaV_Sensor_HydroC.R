52b0b65862b177a344a0bf5f7c3328ccc04a4d3f
jens-daniel-mueller <jens.mueller@io-warnemuende.de>
2019-09-11 17:39:04 +0200

    Proper Zero and Flush labeling

--- 2_Response_Time_determination_TinaV_Sensor_HydroC.R
+++ 2_Response_Time_determination_TinaV_Sensor_HydroC.R
@@ -12,20 +12,21 @@ library(lubridate)
 df <- read_csv(here::here("Data/_summarized_data_files",
                           "Tina_V_Sensor_HydroC_Flush.csv"))
 
+
 df <- df %>%
-  filter(FlushZeroID != 53)
+  filter(Zero_ID != 53)
 
 df <- df %>% 
-  group_by(FlushZeroID, mixing) %>% 
+  group_by(Zero_ID, mixing) %>% 
   mutate(duration_equi = duration- min(duration))
 
 
 # Plot individual Flush periods with exponential fit ----------------------
 
-# for (i in unique(df$FlushZeroID)) {
+# for (i in unique(df$Zero_ID)) {
 # 
 # df_ID <- df %>%
-#   filter(FlushZeroID == i)
+#   filter(Zero_ID == i)
 # 
 # fit <- df_ID %>%
 #   filter(mixing == "equilibration") %>%
@@ -41,7 +42,7 @@ df <- df %>%
 #   geom_vline(xintercept = tau)
 # 
 # ggsave(here::here("/Plots/TinaV/Sensor/HydroC_diagnostics/Response_time_fits",
-#                   paste(i,"_FlushZeroID_HydroC_RT.jpg", sep="")),
+#                   paste(i,"_Zero_ID_HydroC_RT.jpg", sep="")),
 #          width = 10, height = 4)
 # }
 
@@ -50,10 +51,10 @@ df <- df %>%
 
 RT <- df %>% 
   filter(mixing == "equilibration") %>% 
-  group_by(FlushZeroID) %>% 
+  group_by(Zero_ID) %>% 
   do(fit = nls(pCO2_corr ~ SSasymp(duration_equi, yf, y0, log_alpha), data = .)) %>% 
   tidy(fit) %>% 
-  select(FlushZeroID, term, estimate) %>% 
+  select(Zero_ID, term, estimate) %>% 
   spread(term, estimate) %>% 
   select(1,2) %>% 
   mutate(tau = exp(-log_alpha))
@@ -63,17 +64,17 @@ RT <- df %>%
 
 augmented <- df %>% 
   filter(mixing == "equilibration") %>% 
-  group_by(FlushZeroID) %>% 
+  group_by(Zero_ID) %>% 
   do(fit = nls(pCO2_corr ~ SSasymp(duration_equi, yf, y0, log_alpha), data = .)) %>% 
   augment(fit)
 
-# qplot(duration_equi, pCO2_corr, data = augmented, geom = 'point', colour = as.factor(FlushZeroID)) +
+# qplot(duration_equi, pCO2_corr, data = augmented, geom = 'point', colour = as.factor(Zero_ID)) +
 #   geom_line(aes(y=.fitted))
 # 
-# qplot(duration_equi, .resid, data = augmented, geom = 'point', colour = as.factor(FlushZeroID))
+# qplot(duration_equi, .resid, data = augmented, geom = 'point', colour = as.factor(Zero_ID))
 
 augmented_sum <- augmented %>% 
-  group_by(FlushZeroID) %>% 
+  group_by(Zero_ID) %>% 
   summarise(mean_resid = mean(abs(.resid)),
             mean_resid_rel = mean(abs(.resid))/max(pCO2_corr),
             max_pCO2_corr = max(pCO2_corr))
@@ -84,10 +85,10 @@ augmented_sum <- augmented %>%
 
 St_Err <- df %>% 
   filter(mixing == "equilibration") %>% 
-  group_by(FlushZeroID) %>% 
+  group_by(Zero_ID) %>% 
   do(fit = nls(pCO2_corr ~ SSasymp(duration_equi, yf, y0, log_alpha), data = .)) %>% 
   tidy(fit) %>% 
-  select(FlushZeroID, term, std.error) %>% 
+  select(Zero_ID, term, std.error) %>% 
   spread(term, std.error) %>% 
   select(1,2) %>% 
   rename(tau_st_err = log_alpha)
@@ -105,7 +106,7 @@ rm(augmented, augmented_sum, St_Err)
 # Identify residual threshold ---------------------------------------------
 
 RT %>% 
-  ggplot(aes(mean_resid_rel, FlushZeroID, label=FlushZeroID)) +
+  ggplot(aes(mean_resid_rel, Zero_ID, label=Zero_ID)) +
   geom_point(shape=21)+
   scale_fill_viridis_c()+
   geom_label()
@@ -115,8 +116,8 @@ RT %>%
 
 RT %>% 
   filter(mean_resid_rel < 0.0065) %>% 
-  ggplot(aes(FlushZeroID, tau, label=round(mean_resid_rel,4))) +
-  geom_label(data=RT, aes(FlushZeroID, tau, label=round(mean_resid_rel,4)), shape=21, col="red") +
+  ggplot(aes(Zero_ID, tau, label=round(mean_resid_rel,4))) +
+  geom_label(data=RT, aes(Zero_ID, tau, label=round(mean_resid_rel,4)), col="red") +
   geom_point(shape=21)+
   scale_fill_viridis_c()+
   geom_label()
@@ -125,13 +126,13 @@ RT %>%
 
 # Mean tau ----------------------------------------------------------------
 
-max(unique(df[df$date_time < ymd_hms("2018-07-17;13:08:34"),]$FlushZeroID))
-unique(df[df$date_time > ymd_hms("2018-07-17;13:08:34"),]$FlushZeroID)
+max(unique(df[df$date_time < ymd_hms("2018-07-17;13:08:34"),]$Zero_ID))
+unique(df[df$date_time > ymd_hms("2018-07-17;13:08:34"),]$Zero_ID)
 
 
 RT %>% 
   filter(mean_resid_rel < 0.0065) %>% 
-  mutate(pump_power = if_else(FlushZeroID <= 20, "1W", "8W")) %>% 
+  mutate(pump_power = if_else(Zero_ID <= 20, "1W", "8W")) %>% 
   group_by(pump_power) %>% 
   summarise(tau = mean(tau))
 
